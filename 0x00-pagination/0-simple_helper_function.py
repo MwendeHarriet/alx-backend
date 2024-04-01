4d3da35017f04c669720ca178a290bc34ede2013
MwendeHarriet <mwendeharriet@gmail.com>
2024-04-01 15:45:15 +0000

    task0

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Module is a simple helper function: When page numbers are
+    1-indexed, page - 1 is used to align with the 0-indexed
+    nature of lists & arrays."""
+
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """Function returns a tuple of size two containing a start
+    index and an end index corresponding to the range of indexes
+    to return in a list for those particular pagination parameters."""
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+    return start_index, end_index
